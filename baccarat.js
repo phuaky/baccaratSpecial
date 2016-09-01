1499924c8df4b1239d4c0d280c451fac26b67395
Phua Kuan Yu <Phuaky@virus.local>
2016-09-01 13:09:21 +0800

    Can count types, value & multiplier

--- baccarat.js
+++ baccarat.js
@@ -144,10 +144,20 @@ document.getElementById('deal').addEventListener('click', function () {
     deal(player)
     deal(banker)
 
+    // deal(player)
+    // deal(banker)
+
     showCardInHTML(player)
     showCardInHTML(banker)
 
-    natural() // check for natural
+    pHandType.text(natural(player))
+    bHandType.text(natural(banker))
+
+    pValue.text(checkValue(player))
+    bValue.text(checkValue(banker))
+
+    pMultiplier.text(checkTwoCardsMultiplier(player))
+    bMultiplier.text(checkTwoCardsMultiplier(banker))
 
     console.log("player's score " + score(player))
     console.log("banker's scrore " + score(banker))
@@ -166,31 +176,73 @@ document.getElementById('pDraw').addEventListener('click', function () {
     div5.textContent = cardvz[2]
     dealCounter++
   }
+  pValue.text(checkValue(player))
+  pMultiplier.text(checkThreeCardsMultiplier(player))
 })
 
 var counterDeal = 0
 $('#bDraw').on('click', function () {
-  if (dealCounter === 0) {
+  if (counterDeal === 0) {
     deal(banker)
     var div6 = document.getElementById('banker3')
     var cardvz = dth(banker)
     div6.textContent = cardvz[2]
     counterDeal++
   }
+  bValue.text(checkValue(banker))
+  bMultiplier.text(checkThreeCardsMultiplier(banker))
 })
 
 var display = $('#display')
+
 var pHandType = $('#playerHandType')
-var bHandType = $('#bankerHantType')
+var pValue = $('#playerValue')
+var pMultiplier = $('#playerMultiplier')
+
+var bHandType = $('#bankerHandType')
+var bValue = $('#bankerValue')
+var bMultiplier = $('#bankerMultiplier')
+
 document.getElementById('fight').addEventListener('click', function () {
-  display.text(compareValue())
-  pHandType.text(natural())
-  bHandType.text(natural())
+    display.text(findWinner())
 })
 
-// -----CHECK WINNER----- only 2 cards.
+// ----- CHECK MULTIPLIER -----
+function checkTwoCardsMultiplier (person) {
+  if (natural(person)) {
+    if (pair(person)) {
+      return '2x'
+    } else if (suitSuit(person)) {
+      return '2x'
+    }
+    return '1x'
+    }
+  if (pair(person)) {
+    return '2x'
+  } else if (suitSuit(person)) {
+    return '2x'
+  }
+}
 
-function compareValue () {
+function checkThreeCardsMultiplier (person) {
+  if (trips(person)) {
+    return '5x'
+  }  else if (picCube(person)) {
+    return '3x'
+  } else if (suitCube(person)) {
+    return '3x'
+  } else{
+    return 'Multiplier'
+  }
+}
+
+// ----- FIND VALUE -----
+function checkValue (person) {
+  return score(person)
+}
+
+// -----FIND WINNER----- only 2 cards.
+function findWinner () {
   if (pair(player) && pair(banker)) { // CHECK FOR PAIR
     return 'BOTH HAVE PAIRS'
   } else if (pair(player)) {
@@ -239,27 +291,25 @@ function compareValue () {
 }
 
 // --- ALL THE FUNCTIONSSSSS -----
-function natural () {
-  if (score(banker) > 8 && score(player) > 8) {
-    console.log('GAME-END, BOTH NATURAL 9!')
-    return 'NATURAL 9'
-  } else if (score(banker) > 8) {
-    return 'BANKER NATURAL 9'
-  } else if (score(player) > 8) {
-    return 'PLAYER NATURAL 9'
-  } else if (score(banker) > 7 && score(player) > 7) {
-    return 'NATURAL 8'
-  } else if (score(banker) > 7) {
-    return 'BANKER NATURAL 8'
-  } else if (score(player) > 7) {
-    return 'PLAYER NATURAL 8'
-  }
-  // if (suitty(banker) === 200 || suitty(player) === 200) {
-  //   console.log('WITH DOUBLE!!!!!!')
-  //   return 'WITH DOUBLE!!!!!!' // function to end game
-  // }
-}
 
+
+function natural (person) {
+  if (score(person) > 8) {
+    if (pair(person)) {
+      return 'NATURAL PAIR'
+    } else if (suitSuit(person)) {
+      return 'NATURAL SUITED'
+    }
+    return 'NATURAL'
+  } else if (score(person) > 7) {
+    if (pair(person)) {
+      return 'NATURAL PAIR'
+    } else if (suitSuit(person)) {
+      return 'NATURAL SUITED'
+    }
+    return 'NATURAL'
+  }
+}
 function pair (person) { // PAIR FUNCTION
   return getFace(person.cards[0]) === getFace(person.cards[1])
 }
@@ -285,3 +335,69 @@ function showCardInHTML (person) {
     div.textContent = cardz[i]
   }
 }
+
+// GOOD FUNCTIONS, MAYBE CAN STILL USE
+// function natural (person) {
+//   if (score(person) > 8) {
+//     if (pair(person)) {
+//       return 'NATURAL 9 PAIR'
+//     } else if (suitSuit(person)) {
+//       return 'NATURAL 9 SUITED'
+//     }
+//     return 'NATURAL 9'
+//   } else if (score(person) > 7) {
+//     if (pair(person)) {
+//       return 'NATURAL 8 PAIR'
+//     } else if (suitSuit(person)) {
+//       return 'NATURAL 8 SUITED'
+//     }
+//     return 'NATURAL 8'
+//   }
+// }
+// function findWinner () {
+//   if (pair(player) && pair(banker)) { // CHECK FOR PAIR
+//     return 'BOTH HAVE PAIRS'
+//   } else if (pair(player)) {
+//     return 'PLAYER HAS PAIR'
+//   } else if (pair(banker)) {
+//     return 'BANKER HAS PAIR'
+//   }
+//   if (suitSuit(player) && suitSuit(banker)) { // CHECK FOR SUITED
+//     return 'BOTH HAVE SUITED'
+//   } else if (suitSuit(player)) {
+//     return 'PLAYER HAS SUITED'
+//   } else if (suitSuit(banker)) {
+//     return 'BANKER HAS SUITED'
+//   }
+//   if (suitCube(player) && suitCube(banker)) { // CHECK FOR 3 SUITED
+//     return 'BOTH HAVE 3 SUITED'
+//   } else if (suitCube(player)) {
+//     return 'PLAYER HAS 3 SUITED'
+//   } else if (suitCube(banker)) {
+//     return 'BANKER HAS 3 SUITED'
+//   }
+//   if (picCube(player) && picCube(banker)) { // CHECK FOR 3 PICTURES
+//     return 'BOTH HAVE 3 PICTURES'
+//   } else if (picCube(player)) {
+//     return 'PLAYER HAS 3 PICTURES'
+//   } else if (picCube(banker)) {
+//     return 'BANKER HAS 3 PICTURES'
+//   }
+//   if (trips(player) && trips(banker)) { // CHECK FOR TRIPLE
+//     return 'BOTH HAVE TRIPLE'
+//   } else if (trips(player)) {
+//     return 'PLAYER HAS TRIPLE'
+//   } else if (trips(banker)) {
+//     return 'BANKER HAS TRIPLE'
+//   }
+//   if (score(banker) > score(player)) {
+//     return 'BANKER WINS!'
+//   } else if (score(player) > score(banker)) {
+//     return 'PLAYER WINS!'
+//   } else if (score(banker) === score(player)) {
+//     return "It's a DRAW"
+//   } else {
+//     console.log('BuGGGGGGGG')
+//     console.log('banker score is ' + score(banker) + 'player score is ' + score(player))
+//   }
+// }
